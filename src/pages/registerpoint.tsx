d0cd89fed654c0d18bb2298b16541b13cac5d6b2
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 20:30:47 -0300

    :sparkles: Added register button and modal for registering point
    - Added button to open modal for registering a new point
    - Implemented modal with DatePicker for selecting date and time
    - Included 'Registrar' and 'Cancelar' buttons in modal

--- src/pages/registerpoint.tsx
+++ src/pages/registerpoint.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, FormControl, FormLabel } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import ptBR from 'date-fns/locale/pt-BR';
+import "react-datepicker/dist/react-datepicker.css";
+
+registerLocale('pt-BR', ptBR);
 
 const RegisterPointPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [timeRegistered, setTimeRegistered] = useState(new Date());
   const router = useRouter();
   const [userId, setUserId] = useState<number | null>(null);
   const [role, setRole] = useState<string | null>(null);
@@ -28,8 +35,35 @@ const RegisterPointPage = () => {
 
   return (
     <Box>
+      <Button onClick={onOpen} colorScheme="purple" position="fixed" bottom="20px" right="20px">Registrar</Button>
       <Button onClick={handleLogout} colorScheme="red" position="fixed" bottom="20px" left="20px">Sair</Button>
-      {}
+      
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Novo Registro</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Data/Hora</FormLabel>
+              <DatePicker
+                selected={timeRegistered}
+                onChange={(date) => setTimeRegistered(date)}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="dd/MM/yyyy, HH:mm"
+                timeCaption="Hora"
+                locale="pt-BR"
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="purple">Registrar</Button>
+            <Button variant="ghost" onClick={onClose}>Cancelar</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
